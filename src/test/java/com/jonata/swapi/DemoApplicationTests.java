0a9def5570b0f8622e80c4262939dfb2b72a5d54
Jonata Hessa <jonata.hessa@gmail.com>
2020-07-22 18:50:09 -0300

    first commit

---
+++ src/test/java/com/jonata/swapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonata.swapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
